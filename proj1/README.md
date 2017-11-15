536aa56e28bdd4106987e628072d12183544cd77
nickylimjj <nickylimjj.nl@gmail.com>
2017-11-16 00:50:29 +0100

    clarity addded, cmdline args added

---
+++ proj1/README.md
@@ -0,0 +1,3 @@
+```bash
+python factoring.py -h
+```
--- proj1/factoring.py
+++ proj1/factoring.py
@@ -2,6 +2,7 @@ import numpy as np
 import string
 import sys
 import os
+import argparse
 import math as m
 import time
 
@@ -94,6 +95,13 @@ def test_solution(solution, F, N):
 
 if __name__ == "__main__":
 
+    # parse cmdline args
+    parser = argparse.ArgumentParser(description='Factoring Algorithm')
+    parser.add_argument('--generate','-g', action='store_true',
+                        help='generates r values')
+
+    args = parser.parse_args()
+
     # our N
     project_N = 89692892645583511288289L
     test_N1 = 323       # 17 * 19
@@ -101,6 +109,10 @@ if __name__ == "__main__":
     test_N3 = 31741649  # 4621 * 6969
 
     N = test_N1
+
+    prime_file = "prim_2_24.txt"
+    in_file = "in.mat"
+    out_file = "out.mat"
 
     print("factoring project")
     print("----------------")
@@ -110,53 +122,56 @@ if __name__ == "__main__":
     F_size = 1000
     F = []
 
-    filename = "prim_2_24.txt"
-    print("\tloading factorbase {}...".format(filename))
+    if(args.generate):
+        print("\tloading factorbase {}...".format(prime_file))
+        with open(prime_file, 'r') as f:
+            size = 0
+            while size < F_size:
+                line = f.readline()         # 10 per line
+                for word in line.split():
+                    F += [int(word)]
+                    size += 1
+                    
+        # L size specified on website
+        L = 1024
+        B = F[-1] + 1
 
-    with open(filename, 'r') as f:
-        size = 0
-        while size < F_size:
-            line = f.readline()         # 10 per line
-            for word in line.split():
-                F += [int(word)]
-                size += 1
-                
-    B = F[-1] + 1
-    print "\t|F| = {}".format(len(F))
-    print "\t{}-smooth".format(B)
+        print "\t|F| = {}".format(len(F))
+        print "\t{}-smooth".format(B)
+        print "\tL = {}".format(len(F))
 
-    # L size specified on website
-    L = 1024
+        # generate L relations
+        table = np.empty([L, 4]) 
+        
+        # find suitable r values
+        populate_table(table, N, B)
+        print "\t[*] table populated"
 
-    print "\tL = {}".format(len(F))
-    
-    # generate L relations
-    table = np.empty([L, 4]) 
-    
-    # find suitable r values
-    populate_table(table, N, B)
-    print "\t table populated..."
+        # matrix for gaussian elimination
+        M = generate_matrix(table,F)
+        print "\t[*] matrix generated"
 
-    # matrix for gaussian elimination
-    M = generate_matrix(table,F)
+        # save matrix to in.mat
+        with open(in_file, 'w') as f:
+            # save header info
+            m,n = M.shape
+            f.write("{} {}\n".format(m,n))
+            # save matrix M
+            np.savetxt(f, M, fmt='%d', delimiter=' ')
+        print "\t[*] matrix saved to {}".format(in_file)
 
-    # save matrix to M.txt
-    with open("M.txt", "w") as f:
+        # find a nullspace of M that works
+        # we run gauss program
+        os.system("./gauss {} {}".format(in_file, out_file))
+        print "\t[*] gauss program executed. output at {}".format(out_file)
 
-        m,n = M.shape
-        f.write("{} {}\n".format(m,n))
+    with open(out_file,"r") as f:
 
-        for row in M:
-            f.write("{}\n".format(string.strip(str(row),'[] ')))
+        num_soln = f.readline()
 
-    # find a nullspace of M that works
-    # we run gauss program
-    os.system("./gauss M.txt X.txt")
-
-    f = open("X.txt","r")
-    for solution in f.readlines():
-        #TODO test solution
-        p, q = test_solution(solution, N)
-        if (p != 0 and q != 0):
-            print "p={}\tq={}".format(p,q)
+        for solution in f.readlines():
+            #TODO test solution
+            p, q = test_solution(solution, F, N)
+            if (p != 0 and q != 0):
+                print "p={}\tq={}".format(p,q)
         
