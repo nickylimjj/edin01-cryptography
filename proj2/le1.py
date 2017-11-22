275a42c3a360f0729718a8a1e671640d2c331cc3
nickylimjj <nickylimjj.nl@gmail.com>
2017-11-22 14:57:42 +0100

    lab ex 1

--- proj2/le1.py
+++ proj2/le1.py
@@ -5,14 +5,22 @@ from sympy import Poly
 
 x = symbols('x')
 print('part1')
-print(latex(factor(x**23+x**5+1,modulus=2)))
+p1 = Poly(x**23+x**5+1,modulus=2)
+pprint(p1.is_irreducible)
+pprint(p1.is_primitive)
 
 print('part2')
-print(latex(factor(x**23+x**6+1,modulus=2)))
+p2 = Poly(x**23+x**6+1,modulus=2)
+pprint(p2.is_irreducible)
+print(factor(x**23+x**6+1,modulus=2))
 
 print('part3')
-print(latex(factor(x**18+x**3+1,modulus=2)))
+p3= Poly(x**18+x**3+1,modulus=2)
+pprint(p3.is_irreducible)
+pprint(p3.is_primitive)
  
 print('part4')
-print(latex(factor(x**8+x**6+1,modulus=7)))
+p4 = Poly(x**8+x**6+1,modulus=7) 
+pprint(p4.is_irreducible)
+print(factor(x**8+x**6+1,modulus=7))
 
