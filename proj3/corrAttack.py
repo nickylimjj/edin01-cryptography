59d47045a92bef272e2957f446e0af404f393f19
nickylimjj <nickylimjj.nl@gmail.com>
2017-12-06 15:14:20 +0100

    file read and main backbone

--- proj3/corrAttack.py
+++ proj3/corrAttack.py
@@ -6,6 +6,7 @@ def genStream1(K):
 #   INPUT: K (Key)
 
 #   OUTPUT: Stream for LFSR 1 for corresponding K
+    return
 
 
 def genStream2(K):
@@ -13,6 +14,7 @@ def genStream2(K):
 #   INPUT: K (Key)
 
 #   OUTPUT: Stream for LFSR 2 for corresponding K
+    return
 
 
 def genStream3(K):
@@ -20,40 +22,58 @@ def genStream3(K):
 #   INPUT: K (Key)
 
 #   OUTPUT: Stream for LFSR 3 for corresponding K
+    return
 
-def HamDist(genSeq, ourSeq):
+def HamDist(genSeq, givenSeq):
 
 #   INPUT: genSeq (Generated Sequence from LFSR)
-#          ourSeq (Our Sequence)
+#          givenSeq 
 
-#   OUTPUT: Hammering Distance
+#   OUTPUT: Hamming Distance
+    return
 
-def highestP1(ourSeq):
+def highestP1(givenSeq):
 
-#   INPUT:  Our Sequence
+#   INPUT:  Given Sequence
 
 #   OUTPUT: Highest probability for LFSR 1, corresponding Key
 
 #   Outline: Inside a loop, generate a key K, call genStream1(K), call HamDist(...), 
 #            calculate p*, compare with max p*, update max p* and corresponding Key K then output.
+    return
 
-def highestP2(ourSeq):
+def highestP2(givenSeq):
 
-#   INPUT:  Our Sequence
+#   INPUT:  Given Sequence
 
 #   OUTPUT: Highest probability for LFSR 2, corresponding Key
 
 #   Outline: same as highestP1
+    return
 
 
-def highestP3(ourSeq):
+def highestP3(givenSeq):
 
-#   INPUT:  Our Sequence
+#   INPUT:  Given Sequence
 
 #   OUTPUT: Highest probability for LFSR 3, corresponding Key
 
 #   Outline: same as highestP1
+    return
 
 if __name__ == "__main__":
 
-   # Need to import our Sequence 
+    fn = 'task02.txt'
+
+    # Import our Sequence 
+    with open(fn, 'r') as f:
+        givenSeq = "".join([line.strip() for line in f.readlines()])
+
+
+    # calculate most probably key for each LFSR
+    k1 = highestP1(givenSeq)
+    k2 = highestP2(givenSeq)
+    k3 = highestP3(givenSeq)
+
+
+    return (k1,k2,k3)
